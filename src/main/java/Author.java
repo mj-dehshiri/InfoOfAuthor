05bfaa1074e4db84de827f13228748a8f84208c5
m.dehshiri <mj.dehshiri@gmail.com>
2024-01-11 00:02:06 +0330

    add array book

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -1,53 +1,66 @@
+import java.util.Arrays;
+
 public class Author {
-   private String firstName;
-   private String lastName;
-   private int age;
-   private String[] books;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Book[] books;
 
 // constructor
 
 
-   public Author() {
-   }
+    public Author() {
+    }
 
-   public Author(String firstName, String lastName, int age, String[] books) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.age = age;
-      this.books = books;
-   }
+    public Author(String firstName, String lastName, int age, Book[] books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.books = books;
+    }
 
-   //getter and setter
+    //getter and setter
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public int getAge() {
-      return age;
-   }
+    public int getAge() {
+        return age;
+    }
 
-   public void setAge(int age) {
-      this.age = age;
-   }
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-   public String[] getBooks() {
-      return books;
-   }
+    public Book[] getBooks() {
+        return books;
+    }
 
-   public void setBooks(String[] books) {
-      this.books = books;
-   }
+    public void setBooks(Book[] books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", books=" + Arrays.toString(books) +
+                '}';
+    }
 }
+
